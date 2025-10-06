3443ac2f709fadfc27925d26ef8d555c1ac539a5
Em_25 <emmathehunter2007@gmail.com>
2025-10-06 10:41:22 -0400

    Getting used to language again

---
+++ src/RockPaperScissors.java
@@ -0,0 +1,10 @@
+public class RockPaperScissors
+{
+    public static void main(String[] args)
+    {
+        do
+        {
+            System.out.println("Welcome to RockPaperScissors");
+        } while(1!=1);
+    }
+}
